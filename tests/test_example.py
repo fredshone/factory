3b3ece5aad19019e76c0501a6b70f5367243cd2c
fred.shone <fred.shone@arup.com>
2019-08-12 18:31:47 +0100

    still factory supervisor

--- tests/test_example.py
+++ tests/test_example.py
@@ -4,6 +4,7 @@ import pytest
 
 sys.path.append(os.path.abspath('../factory'))
 from example import *
+from factory.factory import combine_reqs
 sys.path.append(os.path.abspath('../tests'))
 
 
@@ -35,6 +36,33 @@ def end():
     return EndProcess(config)
 
 
+req1 = {
+    'a': [1, 2],
+    'b': [1],
+}
+req2 = {
+        'b': [2],
+    }
+req3 = {
+    'b': [3],
+    'c': [1, 2],
+}
+
+
+test_combine_data = [
+    ([req1], req1),
+    ([], None),
+    ([req1, req2], {'a': [1, 2], 'b': [1, 2]}),
+    ([req2, req3], {'b': [2, 3], 'c': [1, 2]}),
+    ([req1, req2, req3], {'a': [1, 2], 'b': [1, 2, 3], 'c': [1, 2]}),
+]
+
+
+@pytest.mark.parametrize("reqs,req", test_combine_data)
+def test_combine_req(reqs, req):
+    assert combine_reqs(reqs) == req
+
+
 def test_pipeline_connection(start, b, c, d, end):
     start.connect(None, [b, c])
     b.connect([start], [d])
@@ -51,6 +79,7 @@ def test_validate(start, b, c, d, end):
     d.connect([b, c], [end])
     end.connect([d], None)
 
-    assert start.requirements ==
+    assert start.requirements() == start.config.requirements()
+    assert b.requirements() == {'a': [1], 'b': [1], 'e': [1]}
     assert start._validate_suppliers()
 
